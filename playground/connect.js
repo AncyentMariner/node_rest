875c46cf8f80a149295965145898474ba62b02e8
ancyentMariner <jeffreyckingswood@gmail.com>
2017-01-05 20:08:28 -0500

    building out calls to mongodb

---
+++ playground/connect.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongo db server');
+  }
+
+  console.log('Connected to mongo db server');
+
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.collection('Users').insertOne({
+    name: 'person'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to add user', err);
+    }
+    // console.log(result);
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});
