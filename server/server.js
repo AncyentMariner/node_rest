cad5b7b1d25151620c927fd0180c53e2974306c9
ancyentMariner <jeffreyckingswood@gmail.com>
2017-01-11 14:44:17 -0500

    added routes for logging users in and out

--- server/server.js
+++ server/server.js
@@ -122,9 +122,29 @@ app.post('/users', (req, res) => {
   });
 });
 
+//POST user/login
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 //sample authentication route
 app.get('/users/auth', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.delete('/users/auth/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -233,3 +233,39 @@ describe('POST /users', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: dummyUsers[1].email, password: dummyUsers[1].password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(dummyUsers[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: dummyUsers[1].email, password: 'password'})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end(done);
+  });
+});
