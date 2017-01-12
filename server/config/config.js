9de1acbd66880267799e5a9c688ba01b39393da1
ancyentMariner <jeffreyckingswood@gmail.com>
2017-01-11 22:51:38 -0500

    fixed variable error in config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || 'test') {
+if (env === 'development' || env === 'test') {
   let config = require('./config.json');
   let envConfig = config[env];
 
